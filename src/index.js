fa11aac819138326658c1aa508971beb669e2b65
Nguyễn Công Long <nguyenconglong95@gmail.com>
2020-08-21 21:01:13 +0700

    * change credential to localhost

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const assetsDirs = {
   app.use(
     cors({
       // origin: process.env.FRONTEND_URL,
-      origin: 'http://localhost',
+      origin: 'http://127.0.0.1:3000',
       credentials: true,
     })
   );
